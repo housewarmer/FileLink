2e5e3727f6cf091e7d6896b7e71ea54545d43b5b
Mark McDonald <mcdonald_mark@apple.com>
2019-04-16 13:58:29 -0700

    Initial Commit

---
+++ FileLink/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  FileLink
+//
+//  Created by Mark McDonald on 4/16/19.
+//  Copyright © 2019 Mark McDonald. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
